c5f4570508b6e4c0ca4549375bd9ca6d5c3fd26d
Milena <emilieguryeva@gmail.com>
2023-08-30 20:13:14 +0300

    add first homework

---
+++ HW1/index.js
@@ -0,0 +1,11 @@
+// Задание 1
+let celsius = 16;
+console.log(`Температура в градусах Цельсия: ${celsius}`);
+
+let fahrenheit = (9 / 5) * celsius + 32;
+console.log(`Температура в градусах Фаренгейта: ${fahrenheit}`);
+
+// Задание 2
+let name = "Milena";
+let admin = name;
+console.log(admin);
